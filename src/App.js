7f4912c67e17a57fde17cd2269ef990d7210af62
Kavinkumar <kavinkumar.sathish@thoughtworks.com>
2021-07-31 18:10:44 +0530

    [Kavin/Thennarasu] Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default App;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+    </div>
+  );
+}
+
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import Enzyme, {shallow} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import App from './App';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('App', function () {
+  it('should have home page', function () {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('header').text()).toEqual('Learn React');
+  });
+});
